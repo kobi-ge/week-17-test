964aee076679e78caa926d98727aa7313799673e
kobi <kodigershuni@gmail.com>
2026-02-12 12:30:56 +0200

    added endpoints and sql connection

---
+++ analytics-api/app/connection.py
@@ -0,0 +1,20 @@
+import mysql.connector 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MysqlManager:
+    def __init__(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host = os.getenv("MYSQL_HOST", "localhost"),
+                user = os.getenv("MYSQL_USER", "root"),
+                password = os.getenv("MYSQL_PASSWORD", ""),
+                port = int(os.getenv("MYSQL_PORT", 3306))
+            )
+            if self.connection.is_connected():
+                self.cursor = self.connection.cursor()
+                print("connection to mysql established")
+        except mysql.connector.Error as e:
+            raise e
